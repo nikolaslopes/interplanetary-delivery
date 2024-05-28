61a0e54fe24e668004f233b26d629cc333c34f48
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-27 22:55:31 -0400

    fix: json-server error on get responses

--- functions/json-server.js
+++ functions/json-server.js
@@ -6,7 +6,20 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-module.exports.handler = (event, context) => {
-  const handler = server.listen(8000);
-  return handler(event, context);
+module.exports.handler = async (event, context) => {
+  const method = event.httpMethod;
+  const url = event.path;
+  const body = event.body;
+
+  const req = { method, url, body };
+  const res = {
+    locals: {},
+  };
+
+  server(req, res, () => {});
+
+  return {
+    statusCode: res.locals.status || 200,
+    body: JSON.stringify(res.locals.data || {}),
+  };
 };
