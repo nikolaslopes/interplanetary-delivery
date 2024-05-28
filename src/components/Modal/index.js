f2a4d257d0fedcce5c5096e9466b7700b2f06173
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-28 02:02:15 -0400

    feat: create modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+
+import {
+  Modal as ModalChakra,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+} from '@chakra-ui/react';
+
+export default function Modal({
+  isOpen,
+  onClose,
+  title,
+  subTitle,
+  primaryButton,
+  secondaryButton,
+}) {
+  return (
+    <ModalChakra isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay bg='rgba(0, 0, 0, 0.5)' backdropFilter='blur(8px)' />
+      <ModalContent background={'gray.600'}>
+        {title ? <ModalHeader>{title}</ModalHeader> : null}
+        <ModalCloseButton />
+
+        <ModalBody>{subTitle ? <Text>{subTitle}</Text> : null}</ModalBody>
+
+        <ModalFooter marginTop='8px'>
+          {primaryButton ? primaryButton : null}
+
+          {secondaryButton ? secondaryButton : null}
+        </ModalFooter>
+      </ModalContent>
+    </ModalChakra>
+  );
+}
+
+Modal.propTypes = {
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  primaryButton: PropTypes.node,
+  secondaryButton: PropTypes.node,
+};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-
+import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
 import {
   Box,
   Button,
@@ -8,21 +9,32 @@ import {
   List,
   ListItem,
 } from '@chakra-ui/react';
-import Lottie from 'lottie-react';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+
+import PageLayout from '../../components/PageLayout';
+import Modal from '../../components/Modal';
 
 import astronautAnimation from '../../assets/animations/astronaut.json';
-import { Link } from 'react-router-dom';
-import PageLayout from '../../components/PageLayout';
-import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
 
 export default function Home() {
   const [addresses, setAddresses] = useState([]);
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+  const [infoModalIsOpen, setInfoModalIsOpen] = useState(false);
+  const [selectedAddressId, setSelectedAddressId] = useState(null);
 
   useEffect(() => {
     const storedAddress = JSON.parse(localStorage.getItem('addresses')) || [];
 
     setAddresses(storedAddress);
   }, []);
+
+  function toggleDeleteModal() {
+    setDeleteModalIsOpen((prevState) => !prevState);
+  }
+
+  function toggleInfoModal() {
+    setInfoModalIsOpen((prevState) => !prevState);
+  }
 
   function saveAddressesToLocalStorage(updatedAddresses) {
     localStorage.setItem('addresses', JSON.stringify(updatedAddresses));
@@ -35,63 +47,136 @@ export default function Home() {
   }
 
   return (
-    <PageLayout title={'Delivery Interplanetário'} backPageLink={false}>
-      <Lottie
-        animationData={astronautAnimation}
-        style={{
-          height: '300px',
-        }}
-        aria-label='Astronaut Animation'
+    <>
+      <Modal
+        isOpen={deleteModalIsOpen}
+        onClose={toggleDeleteModal}
+        title={'Tem certeza que deseja excluir o endereço?'}
+        subTitle={'Esta ação não poderá ser desfeita!'}
+        primaryButton={
+          <Button
+            colorScheme={'whiteAlpha'}
+            variant={'ghost'}
+            marginRight={3}
+            onClick={toggleDeleteModal}
+          >
+            Cancelar
+          </Button>
+        }
+        secondaryButton={
+          <Button
+            colorScheme='red'
+            onClick={() => {
+              handleDeleteAddress(selectedAddressId);
+              toggleDeleteModal();
+            }}
+          >
+            Deletar
+          </Button>
+        }
       />
-      <Box>
-        <Link to='/register'>
-          <Button>Registrar novo endereço</Button>
-        </Link>
-      </Box>
 
-      <Box width={'100%'}>
-        <Heading
-          as='h3'
-          fontSize={'22px'}
-          marginTop={'48px'}
-          marginBottom={'16px'}
-        >
-          Endereços
-        </Heading>
-        <List spacing={3}>
-          {addresses.map((address) => (
-            <ListItem key={address.id}>
-              <Box
-                display='flex'
-                alignItems='center'
-                justifyContent='space-between'
-                paddingY={'8px'}
+      <Modal
+        isOpen={infoModalIsOpen}
+        onClose={toggleInfoModal}
+        title={'Olá! Tudo bem?'}
+        subTitle={
+          'Esta aplicação utiliza o Local Storage do seu navegador para armazenar os endereços cadastrados.'
+        }
+        primaryButton={
+          <Button marginRight={3} onClick={toggleInfoModal}>
+            Ok. Entendi!
+          </Button>
+        }
+      />
+      <PageLayout title={'Delivery Interplanetário'} backPageLink={false}>
+        <Lottie
+          animationData={astronautAnimation}
+          style={{
+            height: '300px',
+          }}
+          aria-label='Astronaut Animation'
+        />
+
+        {addresses.length > 0 && (
+          <Box marginTop={'32px'}>
+            <Link to='/register'>
+              <Button>Registrar novo endereço</Button>
+            </Link>
+          </Box>
+        )}
+
+        <Box width={'100%'}>
+          {addresses.length > 0 ? (
+            <Heading
+              as='h3'
+              fontSize={'22px'}
+              marginTop={'48px'}
+              marginBottom={'16px'}
+            >
+              Endereços
+            </Heading>
+          ) : (
+            <Box
+              marginTop={'32px'}
+              display={'flex'}
+              flexDirection={'column'}
+              alignItems={'center'}
+            >
+              <Heading
+                lineHeight='tall'
+                fontSize={{ base: '18px', sm: '22px', md: '22px' }}
+                textAlign={'center'}
               >
-                <Box marginRight={'8px'}>
-                  <strong>Nome:</strong> {address.name} <br />
-                  <strong>Lote:</strong> {address.lot}
-                </Box>
+                Parece que você não tem nenhum endereço cadastrado! Clique em
+              </Heading>
 
-                <Box display={'flex'}>
+              <Box marginTop='8px'>
+                <Link to='/register'>
+                  <Button>Registrar novo endereço</Button>
+                </Link>
+              </Box>
+            </Box>
+          )}
+
+          <List spacing={3}>
+            {addresses.map((address) => (
+              <ListItem key={address.id}>
+                <Box
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  paddingY={'8px'}
+                >
                   <Box marginRight={'8px'}>
-                    <Link to={`/edit/${address.id}`}>
-                      <IconButton icon={<EditIcon />} />
-                    </Link>
+                    <strong>Nome:</strong> {address.name} <br />
+                    <strong>Lote:</strong> {address.lot}
                   </Box>
 
-                  <IconButton
-                    colorScheme='red'
-                    icon={<DeleteIcon />}
-                    onClick={() => handleDeleteAddress(address.id)}
-                  >
-                    Deletar
-                  </IconButton>
+                  <Box display={'flex'}>
+                    <Box marginRight={'8px'}>
+                      <Link to={`/edit/${address.id}`}>
+                        <IconButton icon={<EditIcon />} />
+                      </Link>
+                    </Box>
+
+                    <IconButton
+                      colorScheme='red'
+                      icon={<DeleteIcon />}
+                      onClick={() => {
+                        setSelectedAddressId(address.id);
+                        toggleDeleteModal();
+                      }}
+                    >
+                      Deletar
+                    </IconButton>
+                  </Box>
                 </Box>
-              </Box>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </PageLayout>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </PageLayout>
+    </>
   );
 }
